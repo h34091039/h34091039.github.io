e96c02439a20eb87147050a861bf5df26cca9dc3
Yung-li Chan <h34091039@gs.ncku.edu.tw>
2023-03-25 00:17:49 +0800

    feat Assignment-5

---
+++ Assignment-5/index.js
@@ -0,0 +1,40 @@
+function count(input)
+{
+    const arr = input.filter((a,b)=> (input.indexOf(a)!== input.lastIndexOf(a) && input.indexOf(a)===b) || input.indexOf(a)=== input.lastIndexOf(a))
+    let dict ={};
+    arr.forEach(key => {
+        let counter = 0;
+        input.forEach((elements)=>{
+            counter = (elements===key) ? counter+1 : counter
+        })
+        dict[key] = counter;
+    })
+    return dict
+}
+let input1 = ["a","b","c","a","c","a","x"];
+console.log(count(input1));
+
+function groupByKey(input)
+{
+    let result = {}
+    for (let i in Object.keys(input))
+    {
+        result[input[i].key] = 0
+        input.forEach((index) =>(
+            result[index.key] = (input[i].key===index.key) ? result[index.key]+index.value : result[index.key]
+        ))
+    } 
+
+    return result
+    
+    
+}
+
+let input2 = [
+    {key: "a", value: 3},
+    {key: "b", value: 1},
+    {key: "c", value: 2},
+    {key: "a", value: 3},
+    {key: "c", value: 5},
+]
+console.log(groupByKey(input2));
